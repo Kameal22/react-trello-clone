56e5d629d0961469ad62a517a76f19100b4504a9
Kamil Czyżewski <kamil.czyzewski96@gmail.com>
2022-04-27 15:10:17 +0200

    Working on main section

--- src/components/mainSection/MainSectionMenu.tsx
+++ src/components/mainSection/MainSectionMenu.tsx
@@ -4,6 +4,16 @@ const MainSectionMenu: React.FC = () => {
   return (
     <div className="mainSectionMenuDiv">
       <h3>Main Section menu</h3>
+
+      <div className="menuBoards"></div>
+      <div className="menuHome"></div>
+
+      <div className="menuWorkspaces">
+        <div className="menuWorkspace">
+          <div className="menuWorkspaceBoard"></div>
+          <div className="menuWorkspaceSettings"></div>
+        </div>
+      </div>
     </div>
   );
 };
