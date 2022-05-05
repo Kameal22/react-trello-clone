91d21b26699df66137e42b6e8131359975a980b7
Kamil Czyżewski <kamil.czyzewski96@gmail.com>
2022-05-05 15:44:35 +0200

    Working on routes with react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import LandingPage from "./components/landingPage/LandingPage";
 import "./styles/App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Workspace from "./components/workspace/Workspace";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/workspace/:params" element={<Workspace />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/workspace/Workspace.tsx
@@ -0,0 +1,7 @@
+import "../../styles/workspaceStyles/workspace.css";
+
+const Workspace: React.FC = () => {
+  return <div className="workspace"></div>;
+};
+
+export default Workspace;
