1fff0d15e85e6b3f7084bc0d5d69f2f1ca0eda47
kamil <kami.czyzewski96@gmail.com>
2022-05-31 23:05:35 +0200

    Working on task details component

--- src/components/popups/TaskDetailsPopUp.tsx
+++ src/components/popups/TaskDetailsPopUp.tsx
@@ -10,12 +10,19 @@ const TaskDetailsPopUp: React.FC = () => {
 
       <p className="taskDetailsListName">In column: To Do</p>
 
-      <div className="taskDetailsDescription">
+      <div className="taskDetailsLabel">
+        <p>Label</p>
+        <div className="taskDetailsLabelDivWithColor"></div>
+      </div>
+
+      <div className="taskDetailsDescriptionDiv">
         <p className="taskDetailsDescriptionHeading">Description</p>
+        <p className="taskDetailsDescription">Description goes here look its very cool OR FORM to enter a description.</p>
       </div>
 
       <div className="taskDetailsActivityDiv">
-        <p>Activity</p>
+        <p className="taskDetailsActivityHeading">Activity</p>
+        <p className="taskDetailsActivityComment">Here is a comment on this task OR input to enter one. Each comment should be a separate component with some logic like deleting etc.</p>
       </div>
     </div>
   );
