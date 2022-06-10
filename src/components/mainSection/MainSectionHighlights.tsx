95d2e7d254c79c128e18d4f6e3a05f638956bac5
Kamil Czyżewski <kamil.czyzewski96@gmail.com>
2022-06-10 12:28:40 +0200

    Added highlighted message from board to main menu

--- src/components/mainSection/MainSectionHighlights.tsx
+++ src/components/mainSection/MainSectionHighlights.tsx
@@ -1,32 +1,54 @@
 import "../../styles/mainSectionStyles/mainSectionHighlights.css";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/Store";
+import { useState, useEffect } from "react";
+
+interface TaskToShowInterface {
+  message: string;
+  label: string;
+  board: string;
+  user: string;
+}
 
 const MainSectionHighlights: React.FC = () => {
+  const [taskToShow, setTaskToShow] = useState<TaskToShowInterface>();
+
   const workspaces = useSelector(
     (state: RootState) => state.workspace.workspace
   );
 
-  // const highlightedWorkspace =
-  //   workspaces[Math.floor(Math.random() * workspaces.length)];
+  const highlightedWorkspace =
+    workspaces[Math.floor(Math.random() * workspaces.length)];
 
-  // const highlightedBoard =
-  //   highlightedWorkspace.workspaceBoards[
-  //     Math.floor(Math.random() * highlightedWorkspace.workspaceBoards.length)
-  //   ];
+  useEffect(() => {
+    if (!highlightedWorkspace) {
+      return;
+    }
 
-  // const highlightedColumn =
-  //   highlightedBoard.boardColumns[
-  //     Math.floor(Math.random() * highlightedBoard.boardColumns.length)
-  //   ];
+    const highlightedBoard =
+      highlightedWorkspace.workspaceBoards[
+        Math.floor(Math.random() * highlightedWorkspace.workspaceBoards.length)
+      ];
 
-  // const highlightedTask = [
-  //   highlightedColumn.columnTasks[
-  //     Math.floor(Math.random() * highlightedColumn.columnTasks.length)
-  //   ],
-  // ];
+    const highlightedColumn =
+      highlightedBoard.boardColumns[
+        Math.floor(Math.random() * highlightedBoard.boardColumns.length)
+      ];
 
-  // console.log(highlightedTask)
+    const highlightedTask =
+      highlightedColumn.columnTasks[
+        Math.floor(Math.random() * highlightedColumn.columnTasks.length)
+      ];
+
+    const label = highlightedTask.taskIndicatorColor;
+    const message = highlightedTask.taskName;
+    const board = highlightedBoard.boardName;
+    const user = highlightedWorkspace.workspaceMember;
+
+    const wholeTask = { label, message, board, user };
+
+    setTaskToShow(wholeTask);
+  }, [highlightedWorkspace]);
 
   return (
     <div className="mainSectionHighlightsDiv">
@@ -38,14 +60,25 @@ const MainSectionHighlights: React.FC = () => {
         </p>
       </div>
 
-      <div className="highlight">
-        <p className="highlightUser">Username</p>
-        <p className="highlightDate">3 days ago</p>
-        <p className="highlightMessage">Add functionality to dropdown menu</p>
-        <p className="highlightBoardInfo">
-          <span>from</span>: TEST board
-        </p>
-      </div>
+      {highlightedWorkspace ? (
+        <div className="highlight">
+          <p className="highlightUser">{taskToShow?.user}</p>
+          <div
+            style={{
+              background: taskToShow?.label,
+              width: "40px",
+              height: "10px",
+              borderRadius: "5px",
+              marginTop: "1vh",
+            }}
+            className="highlightLabel"
+          ></div>
+          <p className="highlightMessage">{taskToShow?.message}</p>
+          <p className="highlightBoardInfo">
+            <span>from</span>: {taskToShow?.board}
+          </p>
+        </div>
+      ) : null}
     </div>
   );
 };
