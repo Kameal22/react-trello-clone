bfb982172b44cb62bd98c01e7dc1471c3c517c5e
Kamil Czyżewski <kamil.czyzewski96@gmail.com>
2022-06-29 14:12:35 +0200

    Added routing to your boards

--- src/components/nav/navMenu/NavUserMenu.tsx
+++ src/components/nav/navMenu/NavUserMenu.tsx
@@ -1,7 +1,9 @@
 import "../../../styles/navStyles/navMenuStyles/userMenu.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { logoutUser } from "../../../redux/features/registerSlice";
 import { showDropdown } from "../../../redux/features/navigationSlice";
+import { RootState } from "../../../redux/Store";
+import { Link } from "react-router-dom";
 
 interface NavUserMenuInterface {
   forwardRef: React.RefObject<HTMLDivElement>;
@@ -9,6 +11,13 @@ interface NavUserMenuInterface {
 
 const NavUserMenu: React.FC<NavUserMenuInterface> = ({ forwardRef }) => {
   const dispatch = useDispatch();
+
+  const workspaces = useSelector(
+    (state: RootState) => state.workspace.workspace
+  );
+
+  const randomWorkspace =
+    workspaces[Math.floor(Math.random() * workspaces.length)];
 
   const setDropdown = (dropdownItem: string) => {
     dispatch(showDropdown({ dropdownItem }));
@@ -29,9 +38,13 @@ const NavUserMenu: React.FC<NavUserMenuInterface> = ({ forwardRef }) => {
       </div>
 
       <div className="userChoiceDiv">
-        <p className="userChoice">
-          Your boards
-        </p>
+        <Link
+          onClick={() => setDropdown("")}
+          className="workspaceMenuLink"
+          to={`/workspace/${randomWorkspace.workspaceId}`}
+        >
+          <p className="userChoice">Your boards</p>
+        </Link>
         <i className="bi bi-clipboard"></i>
       </div>
     </div>
